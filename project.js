66c91bd06441209b1220880ec52dffe785852aba
ttx89 <91700476+ttx89@users.noreply.github.com>
2022-02-06 20:40:31 -0800

    ttx1

---
+++ project.js
@@ -0,0 +1,65 @@
+
+//create elements
+
+
+function setupGame(){
+// 1. add elements 
+// start don't need
+
+// button1 onclick function player1
+
+// button2 onclick function player2
+
+// button3 onclick function initializeGame
+
+//2. Initialize a new game by calling the initializeGame fumction
+    initializeGame()
+}
+
+
+function player1(){
+// restart button disable false
+
+// play dice
+
+// show the step (if == 100 or > 100)
+
+// button1 disable false
+
+// button2 disable true
+
+// change the round
+    
+}
+
+function player2(){
+// play dice
+
+// show the step (if == 100 or > 100)
+
+// button1 disable true
+
+// button2 disable false
+
+   
+}
+function initializeGame(){
+// 1. clear the result
+
+// 2. reset the round 0
+
+// 3. clear the step
+
+// 4. player button disable
+
+// 5. random number
+
+// 6. round number
+
+// 7.restart button disable
+
+}
+
+
+// first prompt player1's  and player2's name
+// make the step table
